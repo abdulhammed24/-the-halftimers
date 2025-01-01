f1e59558c52d946b0d3a1e14e8165b2aeffdd7c2
Abdulhammed <abdlhamd3@gmail.com>
2025-01-02 00:53:53 +0100

    worked on the blog page

--- client/src/app/(pages)/blog/page.tsx
+++ client/src/app/(pages)/blog/page.tsx
@@ -1,105 +1,41 @@
-"use client";
+// "use client";
+
+// import { Suspense } from "react";
+
+// import Hero from "./Hero";
+// import BlogContent from "./BlogContent";
+
+// const Blog = () => {
+//   return (
+//     <>
+//       <Hero />
+//       <Suspense fallback={<div>Loading...</div>}>
+//         <BlogContent />
+//       </Suspense>
+//     </>
+//   );
+// };
+
+// export default Blog;
 
 import { Suspense } from "react";
-import BlogCard from "@/components/BlogCard";
-import postsData from "@/data.json";
-import { useSearchParams } from "next/navigation";
-import Link from "next/link";
+// import postsData from "@/data.json"; // Or fetch from a database
+import BlogContent from "./BlogContent";
 import Hero from "./Hero";
 
-const BlogContent = () => {
-  const posts = postsData.posts;
-  const searchParams = useSearchParams();
-  const category = searchParams.get("category")?.toLowerCase() || "all posts";
+const BlogPage = async () => {
+  // const posts = postsData.posts;
 
-  const filteredPosts =
-    category === "all posts"
-      ? posts
-      : posts.filter((post) => post.category.toLowerCase() === category);
-
-  const handleCategoryChange = (
-    event: React.ChangeEvent<HTMLSelectElement>,
-  ) => {
-    const selectedCategory = event.target.value;
-    window.location.href = `/blog?category=${selectedCategory}`;
-  };
-
-  return (
-    <section className="relative z-20 xl:-mt-[50px]">
-      <div className="mx-auto bg-primary-foreground p-6 lg:w-[calc(100%-160px-160px)] lg:p-10">
-        <div className="mb-6 hidden flex-wrap lg:flex">
-          <Link
-            href="/blog"
-            className={`px-4 py-2 text-sm font-bold italic ${category === "all posts" ? "bg-primary text-white" : "text-foreground"}`}
-          >
-            All Posts
-          </Link>
-
-          <Link
-            href="/blog?category=featured"
-            className={`px-4 py-2 text-sm font-bold italic ${category === "featured" ? "bg-primary text-white" : "text-foreground"}`}
-          >
-            Featured
-          </Link>
-
-          <Link
-            href="/blog?category=football"
-            className={`px-4 py-2 text-sm font-bold italic ${category === "football" ? "bg-primary text-white" : "text-foreground"}`}
-          >
-            Football
-          </Link>
-
-          <Link
-            href="/blog?category=baseball"
-            className={`px-4 py-2 text-sm font-bold italic ${category === "baseball" ? "bg-primary text-white" : "text-foreground"}`}
-          >
-            Baseball
-          </Link>
-
-          <Link
-            href="/blog?category=basketball"
-            className={`px-4 py-2 text-sm font-bold italic ${category === "basketball" ? "bg-primary text-white" : "text-foreground"}`}
-          >
-            Basketball
-          </Link>
-        </div>
-
-        {/* Mobile Navigation */}
-        <div className="mb-6 lg:hidden">
-          <select
-            onChange={handleCategoryChange}
-            value={category}
-            className="bg-white px-4 py-2 text-sm font-bold italic text-foreground"
-          >
-            <option value="all posts">All Posts</option>
-            <option value="featured">Featured</option>
-            <option value="football">Football</option>
-            <option value="baseball">Baseball</option>
-            <option value="basketball">Basketball</option>
-          </select>
-        </div>
-
-        <div className="flex flex-col items-center">
-          <div className="mb-12 w-full space-y-10">
-            {filteredPosts.map((post, index) => (
-              <BlogCard key={index} {...post} />
-            ))}
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
-
-const Blog = () => {
   return (
     <>
       <Hero />
       <Suspense fallback={<div>Loading...</div>}>
+        {/* Blog content here */}
+        {/* <BlogContent posts={posts} /> */}
         <BlogContent />
       </Suspense>
     </>
   );
 };
 
-export default Blog;
+export default BlogPage;
