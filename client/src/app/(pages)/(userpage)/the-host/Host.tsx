cc9020358f9815223aa6a755bc5cc6a88395e6af
Abdulhammed <abdlhamd3@gmail.com>
2025-01-19 19:44:01 +0100

    integrating blog data

--- client/src/app/(pages)/(userpage)/the-host/Host.tsx
+++ client/src/app/(pages)/(userpage)/the-host/Host.tsx
@@ -7,15 +7,6 @@ const Host = () => {
     <section className="relative z-20 xl:-mt-[50px]">
       <Container width="large">
         <div className="bg-primary-foreground p-6 lg:p-10">
-          <div className="mb-10">
-            <p className="text-sm">
-              I'm a paragraph. Click here to add your own text and edit me. It’s
-              easy. Just click “Edit Text” or double click me to add your own
-              content and make changes to the font. I’m a great place for you to
-              tell a story and let your users know a little more about you.
-            </p>
-          </div>
-
           <div>
             <div className="flex flex-wrap items-center gap-5">
               <div className="relative h-32 w-32 flex-shrink-0">
