7ef16ad00c5194bb983d54e86ba1d5d3c76b04bf
Abdulhammed <abdlhamd3@gmail.com>
2025-01-19 21:53:39 +0100

    added some tweak

--- client/src/app/(pages)/(userpage)/(homepage)/components/Podcast.tsx
+++ client/src/app/(pages)/(userpage)/(homepage)/components/Podcast.tsx
@@ -1,5 +1,6 @@
 import Container from "@/components/Container";
 import { shimmer, toBase64 } from "@/utils/imageUtils";
+import * as Tooltip from "@radix-ui/react-tooltip";
 import { ChevronRight } from "lucide-react";
 import Image from "next/image";
 
@@ -18,27 +19,44 @@ const Podcast = () => {
               // sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
             />
           </div>
-          <div className="grid px-6 py-10 lg:grid-cols-2 lg:px-[120px]">
+          <div className="relative z-10 grid px-6 py-10 lg:grid-cols-2 lg:px-[120px]">
             <div className="grid gap-10">
-              <div className="lg:w-[300px]">
+              <div>
                 <h2 className="text-3xl font-bold italic text-primary-foreground">
                   The Halftimers Podcast
                 </h2>
               </div>
-              <div className="w-[200px]">
+              <div>
                 <p className="text-sm text-primary-foreground">
                   Listen to the latest episode & subscribe to all
                 </p>
               </div>
 
-              <button className="flex h-10 w-fit items-center border border-primary-foreground px-5">
-                <span className="text-sm text-primary-foreground">
-                  Listen Now
-                </span>
-                <div>
-                  <ChevronRight className="text-primary-foreground" />
-                </div>
-              </button>
+              <Tooltip.Provider>
+                <Tooltip.Root delayDuration={0}>
+                  <Tooltip.Trigger asChild>
+                    <button className="flex h-10 w-fit items-center border border-primary-foreground px-5">
+                      <span className="text-sm text-primary-foreground">
+                        Listen Now
+                      </span>
+                      <div>
+                        <ChevronRight className="text-primary-foreground" />
+                      </div>
+                    </button>
+                  </Tooltip.Trigger>
+                  <Tooltip.Portal>
+                    <Tooltip.Content
+                      className="z-50 rounded-md bg-black px-2 py-1 text-sm text-white"
+                      side="top"
+                      align="center"
+                      sideOffset={8}
+                    >
+                      Not Available
+                      <Tooltip.Arrow className="fill-black" />
+                    </Tooltip.Content>
+                  </Tooltip.Portal>
+                </Tooltip.Root>
+              </Tooltip.Provider>
 
               <div className="text-sm text-primary-foreground">
                 <p>Subscribe to our podcast via:</p>
@@ -47,13 +65,13 @@ const Podcast = () => {
                 </p>
               </div>
             </div>
-            <div className="relative hidden lg:flex">
+            {/* <div className="relative hidden lg:flex">
               <div className="absolute flex min-h-[350px] w-[400px] items-center justify-center border border-primary-foreground bg-primary p-5 text-center">
                 <p className="text-center text-5xl font-bold italic text-primary-foreground">
                   The Halftimers Podcast
                 </p>
               </div>
-            </div>
+            </div> */}
           </div>{" "}
         </div>
       </Container>
